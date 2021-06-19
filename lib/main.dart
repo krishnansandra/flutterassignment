4244e2c1b140733b65b8bc2bc062b5c16790a506
FlutLab.io <>
2021-06-19 11:15:39 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:todo_hive_example/screens/add_todo/add_todo.dart';
+import 'models/todo.dart';
+import 'client/hive_names.dart';
+
+void main() async {
+  //   hive initialization
+  await Hive.initFlutter();
+  Hive.registerAdapter(TodoAdapter());
+  await Hive.openBox<Todo>(HiveBoxes.todo);
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void dispose() async {
+    Hive.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.brown,
+      ),
+      home: MyHomePage(title: 'My Todo List'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ValueListenableBuilder(
+        valueListenable: Hive.box<Todo>(HiveBoxes.todo).listenable(),
+        builder: (context, Box<Todo> box, _) {
+          if (box.values.isEmpty)
+            return Center(
+              child: Text("Todo list is empty"),
+            );
+          return ListView.builder(
+            itemCount: box.values.length,
+            itemBuilder: (context, index) {
+              Todo res = box.getAt(index);
+              return Dismissible(
+                background: Container(color: Colors.red),
+                key: UniqueKey(),
+                onDismissed: (direction) {
+                  res.delete();
+                },
+                child: ListTile(
+                    title: Text(res.task == null ? '' : res.task),
+                    subtitle: Text(res.note == null ? '' : res.note),
+                    leading: res.complete ? Icon(Icons.check_box) : Icon(Icons.check_box_outline_blank),
+                    onTap: () {
+                      res.complete = !res.complete;
+                      res.save();
+                    }),
+              );
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => AddTodo())),
+        tooltip: 'Add todo',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
